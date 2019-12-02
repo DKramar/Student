9871eb40b58228fe98e30c2eb2bf9dcf41af8050
Denis <denis.kramar@nixsolutions.com>
2019-12-02 18:58:49 +0200

    We express shorted

--- src/main/java/javarush/syntax/level7/WeExpressShorter.java
+++ src/main/java/javarush/syntax/level7/WeExpressShorter.java
@@ -20,24 +20,31 @@ import java.util.ArrayList;
 public class WeExpressShorter {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> listMin = new ArrayList<String>(); // создал список строк
+        ArrayList<String> strings = new ArrayList<String>();
+        int minLenght;
+
         for (int i = 0; i < 3; i++) {
             String s = reader.readLine();
-            list.add(0, s);
+            strings.add(i, s); // добавил строки введенные с клавиатуры в список
         }
+
         //Возможно поможет - https://ru.stackoverflow.com/questions/634119/%d0%9f%d0%be%d0%b8%d1%81%d0%ba-%d0%b8%d0%bd%d0%b4%d0%b5%d0%ba%d1%81%d0%be%d0%b2-%d0%bc%d0%b8%d0%bd%d0%b8%d0%bc%d0%b0%d0%bb%d1%8c%d0%bd%d0%be%d0%b3%d0%be-%d0%b8-%d0%bc%d0%b0%d0%ba%d1%81%d0%b8%d0%bc%d0%b0%d0%bb%d1%8c%d0%bd%d0%be%d0%b3%d0%be-%d1%8d%d0%bb%d0%b5%d0%bc%d0%b5%d0%bd%d1%82%d0%be%d0%b2-%d0%bc%d0%b0%d1%81%d1%81%d0%b8%d0%b2%d0%b0
-        int minLenght = list.get(0).length();
+        minLenght = strings.get(0).length();
         int j = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (minLenght > list.get(i).length()){
-                minLenght = list.get(i).length();
-                //j=0;
-                //listMin.clear();
+        for (int i = 0; i < strings.size(); i++) {
+            if (minLenght > strings.get(i).length()) {
+                minLenght = strings.get(i).length(); //Если есть несколько строк с длиной равной минимальной, то нужно вывести каждую из них с новой строки.
+                j = 0;
+                listMin.clear();
+                listMin.add(j, strings.get(i));
+            } else if (minLenght == strings.get(i).length()) {
+                listMin.add(j++, strings.get(i));
             }
-            /*if(j<min){
-                min = list.get(i);
-            }*/
-            System.out.println();
+        }
+
+        for (int i = 0; i < listMin.size(); i++) { //Программа должна выводить самую короткую строку на экран.
+            System.out.println(listMin.get(i));
         }
     }
 }
