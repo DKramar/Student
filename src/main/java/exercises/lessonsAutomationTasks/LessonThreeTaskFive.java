e6d0120a4014578e019bb620b3929217326dda5b
Denis <denis.kramar@nixsolutions.com>
2020-01-04 17:40:46 +0200

    Exercises. Lesson three. Added task5

---
+++ src/main/java/exercises/lessonsAutomationTasks/LessonThreeTaskFive.java
@@ -0,0 +1,24 @@
+package exercises.lessonsAutomationTasks;
+/*
+Создать простой Java-проект (консольное приложение), которое при запуске выводит в консоль “Hello, world!”.
+Добавить метод printHelloName(), который будет запрашивать у пользователя ввести имя,
+считывать введенное имя с консоли и выводить на экран приветствие “Hello, %name%”.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class LessonThreeTaskFive {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, world!");
+        printHelloName();
+    }
+
+    private static void printHelloName() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your name:");
+        String name = reader.readLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
