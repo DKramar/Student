75573c0c2de0c367f8bcf290a9738c4632892bc9
Denis <denis.kramar@nixsolutions.com>
2020-02-04 15:34:33 +0200

    Added Classes. Task2. Lesson5

--- src/main/java/exercises/lessonsAutomationTasks/lecture5/task2/Demo.java
+++ src/main/java/exercises/lessonsAutomationTasks/lecture5/task2/Demo.java
@@ -1,5 +1,7 @@
 package exercises.lessonsAutomationTasks.lecture5.task2;
-/*
+
+public class Demo {
+    /*
 Создайте иерархию наследования посылок.
 - Базовый класс - Package со следующими элементами: имя, адрес отправителя и получателя, вес (положительное число), цена доставки за грамм (константа double gramCost = 0.1).
 Должен присутствовать метод вычисления стоимости доставки (calculateCost) как произведение веса на цену за грамм.
@@ -16,58 +18,10 @@ package exercises.lessonsAutomationTasks.lecture5.task2;
 создать массив Package[3], в который поместить по одному экземпляру каждого класса, в цикле пройтись по массиву вызывая метод вычисления стоимости посылки для каждого элемента, результат выводить в консоль
  */
 
-public class Package {
-    private String name;
-    private String senderAddress;
-    private String recipientAddress;
-    private double weight;
-    protected static double gramCost = 0.1;
-
-    public Package(String name, String senderAddress, String recipientAddress, double weight) {
-        this.name = name;
-        this.senderAddress = senderAddress;
-        this.recipientAddress = recipientAddress;
-        this.weight = weight;
+    public static void main(String[] args) {
+        Package pack = new Package("Ivan", "address", "address", 200.00);
+        System.out.println(pack.toString() + " DELIVERY WILL COST: " + pack.calculateCost()+ "$");
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSenderAddress() {
-        return senderAddress;
-    }
-
-    public void setSenderAddress(String senderAddress) {
-        this.senderAddress = senderAddress;
-    }
-
-    public String getRecipientAddress() {
-        return recipientAddress;
-    }
-
-    public void setRecipientAddress(String recipientAddress) {
-        this.recipientAddress = recipientAddress;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public double calculateCost() {
-        return this.getWeight() * /*(int)*/Package.gramCost;
-    }
-
-    @Override
-    public String toString() {
-        return "Name: " + name + ". " + "Sender address: " + senderAddress + ". " + "Recipient address: " + recipientAddress + ". " + "Weight: " + weight + " grams. " + "Gram cost: " + gramCost +"$";
-    }
 }
